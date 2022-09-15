ccc833de7fe1e90fd4a10bacf8e0785b3cc18301
pokkode <pokkode2tw@gmail.com>
2022-09-14 23:53:57 -0700

    new folder and files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: variable
+ * @b: variable
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
